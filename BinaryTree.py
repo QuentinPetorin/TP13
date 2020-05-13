4641e6959d2b9033580373f203480437536cebc9
Quentin Petorin <*quentin.petorin@isen-ouest.yncrea.fr*>
2020-05-13 12:26:46 +0200

    commit

--- BinaryTree.py
+++ BinaryTree.py
@@ -145,15 +145,13 @@ class BinaryTree(Node) :
 N21 = Node(21, None, None)
 N18 = Node(18, None, None)
 N3 = Node(3, None, None)
-
 N19 = Node(19, N21, N18)
 N4 = Node(4, None, N3)
 N6 = Node(6, None, None)
-
 N17 = Node(17, N19, None)
 N5 = Node(5, N6, N4)
-
 N12 = Node(12, N17, N5)
+
 Tree = BinaryTree(N12)
 
 object = Tree.Size()
